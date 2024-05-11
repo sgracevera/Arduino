0738f9311d650ae4ca39afb99a450b2221145b95
sgracevera <130788675+sgracevera@users.noreply.github.com>
2024-05-11 23:06:43 +0530

    Update Week4.ino

--- Week4.ino
+++ Week4.ino
@@ -1,13 +1,3 @@
-//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
-// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
-// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
-// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
-// 
-// ArduinoPot
-// 
-// Made by Tania Molina
-// License: MIT
-// Downloaded from: https://circuits.io/circuits/3941296-arduinopot
 
 #define LED 10
 #define POT A0
